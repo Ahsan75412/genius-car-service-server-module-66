af85849e919be9a5986fafeb0c41663ff778d27f
Ahsan Habib <ahsanpub16b@gmail.com>
2022-05-05 23:50:30 +0600

    adding some features and steps

---
+++ __server.deploy.js
@@ -0,0 +1,17 @@
+/*
+  * ONE Time: for my PC
+
+  * Create Heroku App
+  * Verify Email
+  * Install Heroku cli 
+  * login to heroku in terminal
+  * ...................................................
+  *
+  *For Each Project:
+  * ........................
+  * heroku create
+  *
+  *
+  *
+  *
+*/
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion,  ObjectId } = require('mongodb');
-// const { query } = require('express');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,16 +12,51 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//verify access token function
+
+function verifyJWT(req , res , next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message: 'Unauthorized access'});
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return res.status(403).send({message: 'Access denied'});
+        }
+        console.log(decoded);
+        req.decoded = decoded;
+        next();
+    })
+  
+    
+}
+
 
 // connect server to database from "click database and connect to it"
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bewwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// order collection database create on mongoDB
+const orderCollection = client.db('geniusCar').collection('order');
+
 async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
+
+        //Authentication or access token
+
+        app.post('/login' , async(req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET , {
+                expiresIn: '1d'
+            });
+            res.send({accessToken});
+        });
+
+
 
         //get data from database all user API
       app.get('/service',async(req,res)=>{
@@ -58,6 +93,32 @@ async function run(){
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
 
+        });
+
+
+        // order collection API
+
+        app.get('/order', verifyJWT, async(req,res)=>{ 
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+
+            if(email === decodedEmail ){
+                const query = {email : email};
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+
+            }
+            else{
+                res.status(403).send({message: 'Access'});
+            }
+           
+        });
+
+        app.post('/order', async(req,res)=>{
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         });
 
 
